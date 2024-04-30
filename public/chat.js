1363c3c55f6ebfd7386ad87b327fc27ce3758299
Fernando <scolimoski1995@outlook.com>
2024-04-30 09:22:18 -0300

    Switcher socketIO with signalR

--- public/chat.js
+++ public/chat.js
@@ -1,22 +1,39 @@
-var socket = io.connect("http://localhost:4000")
-
 var message = document.getElementById("message");
 var button = document.getElementById("send");
 var username = document.getElementById("username");
 var output = document.getElementById("output");
-console.log(button, username, output);
+
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl("https://localhost:7154/chatHub", {
+        skipNegotiation: true,
+        transport: signalR.HttpTransportType.WebSockets
+    })
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
+
+connection.start().then(function () {
+    console.log("SignalR Connected.");
+}).catch(function (err) {
+    return console.error(err.toString());
+});
 
 button.addEventListener("click", function(){
     console.log("SEND DATA");
-    socket.emit('sendingMessage', { 
-        message: message.value, 
-        username: username.value
+    connection.invoke("SendMessage", username.value, message.value).catch(function (err) {
+        return console.error(err.toString());
     });
+    event.preventDefault();
 })
 
+connection.on("ReceiveMessage", function (user, message) {
+    output.innerHTML += 
+        '<p><strong>' + user + ': </strong>' + message  + '</p>';
+ 
+});
 
-socket.on("broadcastMessage", function(data) {
+
+/*socket.on("broadcastMessage", function(data) {
     console.log(data);
     output.innerHTML += 
         '<p><strong>' + data.username + ': </strong>' + data.message  + '</p>';
-});
+});*/
